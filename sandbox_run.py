a2cfc43045c80f8c8749d510352af276a7dee944
ashutoshmore658 <ashutoshmore658@gmail.com>
2023-06-06 12:55:00 +0530

    modifies sandbox run for memory analysis in json
    Signed-off-by: ashutoshmore658 <ashutoshmore658@gmail.com>

--- sandbox_run.py
+++ sandbox_run.py
@@ -6,5 +6,5 @@ dire="/home/ashutoshreddy/malwares/"
 
 for malfile in os.listdir(dire):
     mal=dire + malfile
-    c_proc=subprocess.Popen(["python3","/home/ashutoshreddy/jupyter/sandbox/sandbox.py",mal,"-f","-m","-t","30"])
+    c_proc=subprocess.Popen(["python3","/home/ashutoshreddy/jupyter/sandbox/sandbox.py",mal,"-f","-M","-t","30"])
     time.sleep(540)
